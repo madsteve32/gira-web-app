41ac7d4f2505c61f5cf32a6897ae1c6022bae01e
madsteve32 <stivan_lyutov@abv.bg>
2021-02-19 19:24:40 +0200

    Gira-app

---
+++ src/main/java/webapp/gira/binding/UserRegisterBindingModel.java
@@ -0,0 +1,60 @@
+package webapp.gira.binding;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserRegisterBindingModel {
+
+    private String username;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    public UserRegisterBindingModel() {
+    }
+
+    @NotBlank(message = "Username blank cannot be empty")
+    @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters")
+    public String getUsername() {
+        return username;
+    }
+
+    public UserRegisterBindingModel setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    @NotBlank(message = "Email blank cannot be empty")
+    @Email(message = "Email address must be valid")
+    public String getEmail() {
+        return email;
+    }
+
+    public UserRegisterBindingModel setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @NotBlank(message = "Password blank cannot be empty")
+    @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters")
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRegisterBindingModel setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @NotBlank(message = "Password blank cannot be empty")
+    @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters")
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public UserRegisterBindingModel setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        return this;
+    }
+}
